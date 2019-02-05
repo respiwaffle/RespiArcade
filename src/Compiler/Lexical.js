bc0403cdbabec257f8284d0cdbc8611928814397
eniallator <eniallatormc@gmail.com>
2019-02-05 12:37:10 +0000

    Split up strings from the tokens since there will be a special case for them. Made a list of keywords for various constructs

--- src/Compiler/Lexical.js
+++ src/Compiler/Lexical.js
@@ -4,7 +4,8 @@ Lexical analysis of the script src code happens here
 Priority for matching:
 1. comments and strings
 2. remove unecessary whitespace (though since significant whitespace, not much to remove)
-3. the rest of the data types
+3. keywords
+4. the rest of the data types
 */
 
 const comments = {
@@ -12,8 +13,14 @@ const comments = {
     multiLine: /\/\*[\s\S]*\*\//g
 }
 
+const strings = {
+    singleLine: /(["'])(\1|.*?([^\\]\1))/g,
+    multiLine: /"{3}[\s\S]*"{3}/g
+}
+
+const keywords = ['if', 'elseif', 'else', 'for', 'while', 'break', 'continue', 'function', 'return']
+
 const tokens = {
-    string: /(["'])(\1|[\s\S]*?([^\\]\1))/g,
     variable: /[_a-z][_a-z\d]*/gi,
     number: /\d+(\.\d+)?([+-]?e\d+)?/g,
     boolean: /true|false/g
