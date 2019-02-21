0c3f523f19b851165103b0852934164e9d410889
eniallator <eniallatormc@gmail.com>
2019-02-21 12:42:44 +0000

    Reworked the lexical analysis and implemented overlapping tokens matching

--- src/Compiler/Lexical.js
+++ src/Compiler/Lexical.js
@@ -8,15 +8,12 @@ Priority for matching:
 4. the rest of the data types
 */
 
-const comments = {
-    singleLine: /\/\/.*$/gm,
-    multiLine: /\/\*[\s\S]*\*\//g
-}
-
-const strings = {
-    singleLine: /(["'])(\1|.*?([^\\]\1))/g,
-    multiLine: /"{3}[\s\S]*"{3}/g
-}
+const overlappingTokens = [
+    { type: 'string', regex: /(["'])(\1|.*?([^\\]\1))/g },
+    { type: 'string', multiLine: true, regex: /"{3}[\s\S]*"{3}/g },
+    { type: 'comment', remove: true, regex: /\/\/.*$/gm },
+    { type: 'comment', multiLine: true, remove: true, regex: /\/\*[\s\S]*\*\//g }
+]
 
 // Array so that some token matches will have priority over others
 const tokens = [
@@ -30,3 +27,53 @@ const tokens = [
         operator: /(\+|-|\/|\*)/g
     }
 ]
+
+function addToSymbolTable(item, symbolTable) {
+    let index = 0
+    for (let otherItem of symbolTable) {
+        if (otherItem.pos[0] > item.pos[0]) break
+        index++
+    }
+    symbolTable.splice(index, 0, item)
+}
+
+function stripOverlappingTokens(symbolTable) {
+    let lastItem = symbolTable[0]
+    let newSymbolTable = [lastItem]
+
+    for (let i = 1; i < symbolTable.length; i++) {
+        if (lastItem.pos[1] < symbolTable[i].pos[0]) {
+            newSymbolTable.push(symbolTable[i])
+            lastItem = symbolTable[i]
+        }
+    }
+
+    return newSymbolTable
+}
+
+function parseOverlappingTokens(rawSrc) {
+    const symbolTable = []
+
+    for (let tokenType of overlappingTokens) {
+        let match
+        while ((match = tokenType.regex.exec(rawSrc))) {
+            const item = { input: match[0], type: tokenType.type, pos: [match.index, match.index + match[0].length] }
+            addToSymbolTable(item, symbolTable)
+        }
+    }
+
+    const strippedSymbolTable = stripOverlappingTokens(symbolTable)
+
+    return { symbolTable: strippedSymbolTable, updatedSrc: rawSrc }
+}
+
+function firstStageLexical(rawSrc) {
+    const { symbolTable, updatedSrc } = parseOverlappingTokens(rawSrc)
+    console.log(symbolTable)
+}
+
+function lexicalAnalysis(rawSrc) {
+    firstStageLexical(rawSrc)
+}
+
+lexicalAnalysis('// "string inside comment"')
